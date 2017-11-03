84b9dce8dc29310b153cf39ce62a54f6d51f3da9
Arik Waisman <awaisman@observer.com>
2017-11-03 14:16:19 -0400

    adding cors

--- router.js
+++ router.js
@@ -4,13 +4,13 @@ const passport = require('passport');
 
 //by default passport tries to create a cookie, but since we use tokens we set it to false
 const requireAuth = passport.authenticate('jwt', { session: false});
-const requireSignin = passport.authenticate('local', { session: false })
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app){
 
 	app.get('/', requireAuth, function(req, res){
-		res.send({hi: 'there'});
-	})
+		res.send({ message: 'super secret code is abc123' });
+	});
 	app.post('/signin', requireSignin, Authentication.signin);
 	app.post('/signup', Authentication.signup)
 
